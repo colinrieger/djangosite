31ba6a0a598cc80cd25652051de4d806c825fc45
Colin Rieger <rieger1085@gmail.com>
2018-08-22 13:48:15 -0600

    Redirect project index to wishlist for now.

--- djangosite/urls.py
+++ djangosite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='wishlist/')),
     path('admin/', admin.site.urls),
     path('wishlist/', include('wishlist.urls')),
 ]
